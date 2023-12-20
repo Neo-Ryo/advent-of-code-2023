8111385815e8e43ea352da0988f39ed58be259de
Neo Ryo <marco.sch4064@gmail.com>
2023-12-20 11:51:23 +0100

    yay

--- day-16/index.py
+++ day-16/index.py
@@ -5,9 +5,6 @@ with open("day-16/input.txt") as f:
     inputFile = f.read()
 
 
-beams = []
-energized = [];
-
 ## try new approch by duplicating beams rather than recursive calls
 class Beam:
     x = 0
@@ -40,22 +37,22 @@ class Beam:
 
 
 
-def add_energized(beam):
-    nrgz = "{}{}{}".format(beam.y, beam.x, beam.direction)
+def add_energized(beam, energized):
+    nrgz = "{}|{}{}".format(beam.y, beam.x, beam.direction)
     if not nrgz in energized:
         energized.append(nrgz)
         return False
     else:
         return True
                        
-def del_beam(beam):
+def del_beam(beam, beams):
         beamHash = beam.__hash__()
         for i in range(len(beams)):
             if beams[i].__hash__() == beamHash:
                 del beams[i]
                 break
 
-def make_new_beam(beam, direction):
+def make_new_beam(beam, direction, beams):
     newBeam = Beam()
     if direction == "N":
         newBeam.y = beam.y-1
@@ -73,22 +70,28 @@ def make_new_beam(beam, direction):
     beams.append(newBeam)
     return newBeam
 
-def one(file: str):
+def one(file: str, start: { "x": int, "y": int, "direction": str}):
+    print(start)
     grid = []
+    beams = []
+    energized = [];
     for l in file.split("\n"):
         grid.append([*l])
-
     beam = Beam()
+    beam.x = start["x"]
+    beam.y = start["y"]
+    beam.direction = start["direction"]
     beams.append(beam)
+
     while len(beams):
         for bm in beams:
             if bm.is_out(grid):
-                del_beam(bm)
+                del_beam(bm, beams)
                 continue
             else:
-                is_already_nrgz = add_energized(bm)
+                is_already_nrgz = add_energized(bm, energized)
                 if is_already_nrgz:
-                    del_beam(bm)
+                    del_beam(bm, beams)
                     continue
                 else:
                     # print(bm.y, bm.x)
@@ -102,7 +105,7 @@ def one(file: str):
                                 bm.change_direction("S")
                             elif spot == "|":
                                 bm.change_direction("N")
-                                make_new_beam(bm, "S")
+                                make_new_beam(bm, "S", beams)
                             else:
                                 bm.move()
                         case "S":
@@ -112,7 +115,7 @@ def one(file: str):
                                 bm.change_direction("E")
                             elif spot == "-":
                                 bm.change_direction("W")
-                                make_new_beam(bm, "E")
+                                make_new_beam(bm, "E", beams)
                             else:
                                 bm.move()
                         case "W":
@@ -122,7 +125,7 @@ def one(file: str):
                                 bm.change_direction("N")
                             elif spot == "|":
                                 bm.change_direction("S")
-                                make_new_beam(bm, "N")
+                                make_new_beam(bm, "N", beams)
                             else:
                                 bm.move()
                         case "N":
@@ -132,17 +135,18 @@ def one(file: str):
                                 bm.change_direction("W")
                             elif spot == "-":
                                 bm.change_direction("E")
-                                make_new_beam(bm, "W")
+                                make_new_beam(bm, "W", beams)
                             else:
                                 bm.move()
-    print(energized)
+    # print(energized)
     single_points = []
     for nrgz in energized:
         x = nrgz[:-1]
         if not x in single_points:
             single_points.append(x)
-    print(single_points)
+    # print(single_points)
     print(len(single_points))
+    return len(single_points)
     # new_grid = []
     # for i in range(len(grid)):
     #     new_grid.append([])
@@ -155,5 +159,45 @@ def one(file: str):
     # print("\n".join(lines))
     
 
-# one(test)
-one(inputFile)
+def two(file: str):
+    grid = []
+    for l in file.split("\n"):
+        grid.append([*l])
+    
+    #TOP left ->
+    #TOP right <-
+    #BOTTOM left ->
+    #BOTTOM right <-
+    top_left = [{ "x": 0, "y": 0, "direction": "E" }, { "x": 0, "y": 0, "direction": "S" }]
+    top_right = [{ "x": len(grid[0]) - 1, "y": 0, "direction": "W" }, { "x": len(grid[0]) - 1, "y": 0, "direction": "S" }]
+    bot_left = [{ "x": 0, "y": len(grid) - 1, "direction": "E" }, { "x": 0, "y": len(grid) - 1, "direction": "N" }]
+    bot_right = [{ "x": len(grid[0]) - 1, "y": len(grid) - 1, "direction": "W" }, { "x": len(grid[0]) - 1, "y": len(grid) - 1, "direction": "N" }]
+    res = []
+    for i in range(len(grid[0])):
+        #TOP
+        if i == 0:
+            for t in top_left:
+                res.append(one(file, t))
+        elif i == len(grid[0]) - 1:
+            for t in top_right:
+                res.append(one(file, t))
+        else:
+            res.append(one(file, { "x": i, "y": 0, "direction": "S" }))
+    for i in range(len(grid[len(grid) - 1])):
+        #BOT
+        if i == 0:
+            for t in bot_left:
+                res.append(one(file, t))
+        elif i == len(grid[0]) - 1:
+            for t in bot_right:
+                res.append(one(file, t))
+        else:
+            res.append(one(file, { "x": i, "y": len(grid[len(grid) - 1]) - 1, "direction": "N" }))
+
+    print(res)
+    print(max(res))   
+
+# one(test, { "x": 0, "y": 0, "direction": "S"})
+# one(inputFile)
+# two(test)
+two(inputFile)
